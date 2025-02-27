d06441f74916013a242e7638490ea5adbfa96dba
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-02-27 20:24:55 +0100

    [TEST] Changes to encrypted message should throw an exception

--- src/test/java/org/example/crypto/AesGcmCryptoTest.java
+++ src/test/java/org/example/crypto/AesGcmCryptoTest.java
@@ -1,12 +1,32 @@
 package org.example.crypto;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Base64;
 
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AesGcmCryptoTest {
+
+    @Test
+    void changesShouldNotBeAccepted() {
+        String myMessage = "Rabbits running across the meadow";
+        KeyGenerator keyGenerator = new KeyGenerator();
+        Key key = keyGenerator.getSecureRandomKey( "SomeSecretPass" );
+
+        AesGcmCrypto aesGcmCrypto = new AesGcmCrypto(key);
+        String encryptedBase64Message = aesGcmCrypto.encryptToEncodedBase64( myMessage );
+        byte[] encryptedBytes = Base64.getDecoder().decode( encryptedBase64Message );
+        encryptedBytes[14] = 0x41;
+        String manipulatedMessage = Base64.getEncoder().encodeToString( encryptedBytes );
+
+        assertThatThrownBy( () -> aesGcmCrypto.decryptFromBase64( manipulatedMessage ) )
+            .hasMessageContaining( "Tag mismatch" );
+    }
+
 
     @Test
     void aesGcmTest() {
